049957b7c8a23ad2e064225c7c21e26ee809e93e
Javinas <jairo.vinas@gmail.com>
2021-10-19 17:53:23 -0500

    Add: update register.py

--- registers.py
+++ registers.py
@@ -85,7 +85,6 @@ def sql_get_email(correo):
                 else:
                     return True    
             except Error:
-<<<<<<< HEAD
                 print(Error)  
 
 def sql_get_user_info(usuario):
@@ -143,6 +142,3 @@ def sql_get_email_profile(usuario,correo):
                     return False   
             except Error:
                 print(Error)              
-=======
-                print(Error)           
->>>>>>> 953fed156032ab0ec9715d1993d3b23cdd333721
